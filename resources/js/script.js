d094ee79682d5214ffb8872742c48aa43e78e724
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-08-08 23:02:25 -0700

    added mobile nav section and responsiveness for mobile devices at tablet size

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function(){
+$('.js--section-features').waypoint(function(direction){
+  if (direction == "down") {
+    $('nav').addClass('sticky');
+  } else {
+    $('nav').removeClass('sticky');
+  }
+});
