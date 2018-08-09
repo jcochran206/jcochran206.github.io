58fa251f0185f64e68425d5bddf225737b559ecc
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-08-08 23:12:15 -0700

    added toggle to nav section starting at about section

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-$('.js--section-features').waypoint(function(direction){
+$('.js--aboutme').waypoint(function(direction){
   if (direction == "down") {
     $('nav').addClass('sticky');
   } else {
     $('nav').removeClass('sticky');
   }
+});
+
+
+/* mobile nav toggle */
+/* Mobile navigation */
+$('.js--nav-icon').click(function(){
+  var nav = $('.js--main-nav');
+  var icon = $('.js--nav-icon ion-icon');
+
+  nav.slideToggle(200);
+
+  });
+
 });
